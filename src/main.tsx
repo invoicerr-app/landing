e4d174e3d0b766314b77cf2a0d3061d9f32378da
Roméo Chevrier <impr.visible@gmail.com>
2025-11-11 13:43:47 +0100

    ♻️ refactor: Restructure App component for improved readability and window size handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,63 @@
 import './index.css'
 
+import { useEffect, useState } from 'react';
+
+import { AnimatedPdf } from './components/animated-pdf';
 import Header from './components/header'
 import Home from './components/home'
 import { PostIt } from './components/post-it'
 import clientApproval from './assets/writings/client-approval-pending.svg';
 import { createRoot } from 'react-dom/client'
 import depositWaiting from './assets/writings/30p-deposit-waiting.svg';
+import doc1 from './assets/documents/doc-1.webp';
+import doc2 from './assets/documents/doc-2.webp';
+import doc3 from './assets/documents/doc-3.webp';
 import firstMonthUnpaid from './assets/writings/first-month-unpaid.svg';
 
-createRoot(document.getElementById('root')!).render(
-  <main className='min-h-screen h-[500dvh]'>
-    <Header />
-    <Home />
-    <section id='content-overlay'>
-      <PostIt left={15} top={15} color="pink" rotation={20}>
-        <img src={clientApproval} alt="Client Approval Pending" className="w-full h-full mx-auto mb-2" />
-      </PostIt>
-      <PostIt left={75} top={30} color="yellow" rotation={-24}>
-        <img src={depositWaiting} alt="30% Deposit Waiting" className="w-full h-full mx-auto mb-2" />
-      </PostIt>
-      <PostIt left={25} top={65} color="blue" rotation={-20}>
-        <img src={firstMonthUnpaid} alt="First Month Unpaid" className="w-full h-full mx-auto mb-2 -rotate-16" />
-      </PostIt>
-    </section>
-  </main>
-)
+function App() {
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const handleResize = () => setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const boxes = [
+    { asset: doc1, startX: 0, startY: -windowSize.height * 0.975, startRot: 13.6 },
+    { asset: doc3, startX: windowSize.width * 0.85, startY: -windowSize.height, startRot: -25 },
+    { asset: doc2, startX: (windowSize.width / 100) * 3, startY: -windowSize.height / 1.75, startRot: -30 },
+  ];
+
+  return (
+    <main className='min-h-screen h-[500dvh]'>
+      <Header />
+      <Home />
+      <section className='fixed w-screen h-screen'>
+        {boxes.map((box, index) => (
+          <AnimatedPdf
+            key={index}
+            startX={box.startX}
+            startY={box.startY}
+            startRot={box.startRot}
+            asset={box.asset}
+            assetAlt={`Animated PDF: ${index + 1}`}
+          />
+        ))}
+      </section>
+      <section id='content-overlay'>
+        <PostIt left={25} top={15} color="pink" rotation={20}>
+          <img src={clientApproval} alt="Client Approval Pending" className="w-full h-full mx-auto mb-2" />
+        </PostIt>
+        <PostIt left={75} top={30} color="yellow" rotation={-24}>
+          <img src={depositWaiting} alt="30% Deposit Waiting" className="w-full h-full mx-auto mb-2" />
+        </PostIt>
+        <PostIt left={25} top={65} color="blue" rotation={-20}>
+          <img src={firstMonthUnpaid} alt="First Month Unpaid" className="w-full h-full mx-auto mb-2 -rotate-16" />
+        </PostIt>
+      </section>
+    </main>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
