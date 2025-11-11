74d8688689707dee70d93bac80bf63b85959609f
Roméo Chevrier <impr.visible@gmail.com>
2025-11-11 12:39:35 +0100

    🎉 init: Create Home component with introductory text and layout

---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <section className="w-full h-screen flex flex-col justify-center items-center gap-8">
+            <h1 className="font-extrabold text-5xl text-center">
+                Never chase<br />invoices again.
+            </h1>
+            <p className="text-xl text-center max-w-xl">
+                Invoicerr helps freelancers and teams create,<br />send, and track invoices effortlessly.
+            </p>
+        </section>
+    );
+}
