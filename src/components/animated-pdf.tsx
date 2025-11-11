c0254aa9dc786cc7af4b80823651d5734d4f8695
Roméo Chevrier <impr.visible@gmail.com>
2025-11-11 17:03:21 +0100

    ➕ dep-add: Replace framer-motion with motion for animation handling

--- src/components/animated-pdf.tsx
+++ src/components/animated-pdf.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
 import { useEffect, useState } from "react";
 
 interface AnimatedPdfProps {
--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -1,6 +1,7 @@
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
 import { useEffect, useState } from "react";
 
+import { CheckCircle } from "lucide-react";
 import phoneImage from "../assets/phone.png";
 
 export default function Phone() {
@@ -25,26 +26,50 @@ export default function Phone() {
 
     const { scrollY } = useScroll();
 
-    const yLinear = useTransform(
+    const yPhoneLinear = useTransform(
         scrollY,
         [1200, 1800],
         [windowSize.height * 1, windowSize.height * -0.05]
     );
+    const yPhone = useSpring(yPhoneLinear, { stiffness: 120, damping: 25 });
 
-    const y = useSpring(yLinear, { stiffness: 120, damping: 25 });
+    const opacitySuccessCard = useTransform(
+        scrollY,
+        [8400, 8600, 11000],
+        [0, 1, 1]
+    );
+
+    const opacitySuccessCardSpring = useSpring(opacitySuccessCard, { stiffness: 120, damping: 25 });
 
     return (
-        <motion.img
-            src={phoneImage}
-            alt="Phone Mockup"
-            style={{
-                width,
-                x: (windowSize.width - width) / 2,
-                y,
-                scale: 1.3,
-                position: "fixed",
-                bottom: 0,
-            }}
-        />
+        <>
+            <motion.div
+                className="bg-green-400 flex flex-col items-center justify-center"
+                style={{
+                    width: width * 0.85,
+                    height: width * 2.16 * 0.85,
+                    marginTop: "-39%",
+                    x: (windowSize.width - width) / 2 + width * 0.075,
+                    y: windowSize.height * -0.05,
+                    opacity: opacitySuccessCardSpring,
+                }}
+            >
+                <CheckCircle className="mx-auto mt-10 mb-4" size={64} color="white" />
+                <h2 className="text-4xl font-bold text-white">Success</h2>
+                <h3 className="text-xl font-medium text-white">You’ve just made<br />invoicing simple.</h3>
+            </motion.div>
+            <motion.img
+                src={phoneImage}
+                alt="Phone Mockup"
+                style={{
+                    width,
+                    x: (windowSize.width - width) / 2,
+                    y: yPhone,
+                    scale: 1.3,
+                    position: "fixed",
+                    bottom: 0,
+                }}
+            />
+        </>
     );
 }
