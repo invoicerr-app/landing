0c3b5927b1b171c359b0787c36d492bb37c47e05
Roméo Chevrier <impr.visible@gmail.com>
2025-11-11 15:10:24 +0100

    🎉 init: Add Phone component with responsive behavior and scroll animation

---
+++ src/components/phone.tsx
@@ -0,0 +1,52 @@
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import phoneImage from "../assets/phone.png";
+
+export default function Phone() {
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const scaleFactor = (windowSize.height / 885) * 1.1;
+    const width = 400 * scaleFactor;
+
+    // Récupère la position du scroll
+    const { scrollY } = useScroll();
+
+    // Définition de la position verticale : part du bas vers le centre
+    const yLinear = useTransform(
+        scrollY,
+        [1200, 1800], // plage de scroll
+        [windowSize.height * 1, windowSize.height * -0.05] // déplacement vertical
+    );
+
+    const y = useSpring(yLinear, { stiffness: 120, damping: 25 });
+
+    return (
+        <motion.img
+            src={phoneImage}
+            alt="Phone Mockup"
+            style={{
+                width,
+                x: (windowSize.width - width) / 2,
+                y,
+                scale: 1.3,
+                position: "fixed",
+                bottom: 0,
+            }}
+        />
+    );
+}
