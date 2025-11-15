86b1823b3b71ebb8b936af63aca0c9cb712c98f0
J72085 <romeo.chevrier@enedis.fr>
2025-11-15 15:19:45 +0100

    ♻️ refactor: Remove prebuild script from package.json for cleaner configuration and streamline main.tsx rendering logic.

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,4 @@ import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 import './index.css';
 
-const container = document.getElementById('root')!
-createRoot(container).render(<Routes />)
+createRoot(document.getElementById('root')!).render(<Routes />)
